8aeb8fe121b8794eb1f797d6408751088ea2911c
florinrelea <florinrelea65@gmail.com>
2022-08-21 21:54:53 +0300

    More progress

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7049");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var input = new HelloRequest { Name = "Tim" };
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine("Reply message {0}", reply.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
