d01238738313d25f95d096e3f6182653c1a8fc94
florinrelea <florinrelea65@gmail.com>
2022-08-22 00:31:00 +0300

    Added todo

--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -16,3 +16,8 @@ app.MapGrpcService<CustomersService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+/*
+    TODO:
+        - Learn when the connection opens, when it starts and for how long it stays open
+*/
