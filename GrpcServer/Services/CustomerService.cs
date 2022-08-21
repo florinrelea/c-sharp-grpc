850d1866f2b7a250705bb29acce48dc09b290b37
florinrelea <florinrelea65@gmail.com>
2022-08-21 22:23:32 +0300

    More service and stuff

---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+            } else
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
